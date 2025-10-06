dd9afecc3cb00cd83afbe4fde9dbfefae94e9361
YagilAharoni <yagilaharoni@gmail.com>
2025-10-06 18:28:07 +0300

    add the calculator source code

---
+++ calc.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	char op;
+	double num1;
+	double num2;
+	double result;
+	cout << "Enter either (+ - * /): ";
+	cin >> op;
+	cout << "enter number 1:";
+	cin >> num1; 
+	cout << "enter number 2:";
+	cin >> num2;
+	switch (op) {
+		case '+' :
+			result = num1 + num2;
+			cout << result;
+			break;
+		case '-':
+			result = num1 - num2;
+			cout << result;
+			break;
+		case '*':
+			result = num1 * num2;
+			cout << result;
+			break;
+		case '/':
+			result = num1 / num2;
+			cout << result;
+			break;
+		default:
+			cout << " please enter only one of the op above:";
+	
+	}
+	return 0;
+}
