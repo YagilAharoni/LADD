99888c724f8b218633ef217a80b44e8c2ba86726
YagilAharoni <yagilaharoni@gmail.com>
2025-10-18 18:27:02 +0000

    fix code

--- calc.cpp
+++ calc.cpp
@@ -6,28 +6,28 @@ int main() {
 	double num1;
 	double num2;
 	double result;
+	cout << "enter number 1: ";
+	cin >> num1; 
 	cout << "Enter either (+ - * /): ";
 	cin >> op;
-	cout << "enter number 1:";
-	cin >> num1; 
 	cout << "enter number 2:";
 	cin >> num2;
 	switch (op) {
 		case '+' :
 			result = num1 + num2;
-			cout << result;
+			cout << result << "/n";
 			break;
 		case '-':
 			result = num1 - num2;
-			cout << result;
+			cout << result << "/n";
 			break;
 		case '*':
 			result = num1 * num2;
-			cout << result;
+			cout << result << "/n";
 			break;
 		case '/':
 			result = num1 / num2;
-			cout << result;
+			cout << result << "/n";
 			break;
 		default:
 			cout << " please enter only one of the op above:";
