43ad3476235ccef5f078102c20b65efdd08ec640
YagilAharoni <yagilaharoni@gmail.com>
2025-10-18 18:31:15 +0000

    added dynamic linkage to my calc app

--- calc.cpp
+++ calc.cpp
@@ -2,6 +2,14 @@
 #include <cmath>
 using namespace std;
 int main() {
+	void *handle = dlopen("./libladd.so", RTLD_NOW | RTLD_GLOBAL);
+    if (!handle) {
+        fprintf(stderr, "Failed to load libladd.so: %s\n", dlerror());
+        return 1;
+    }
+
+    // The constructor in ladd.so will run automatically
+    printf("libladd.so loaded successfully\n");
 	char op;
 	double num1;
 	double num2;
